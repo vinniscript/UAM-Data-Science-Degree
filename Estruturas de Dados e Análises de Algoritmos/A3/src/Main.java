7c601bf4c695c5bef2f1aa8ba1efb28b05ed606c
Vinicius Pereira de Sousa <vinicius.sousa@castgroup.com.br>
2025-11-06 18:22:25 -0300

    Idealizando a função de testes. Não tem nada ainda. Preciso pensar em como vai funcionar.

--- Estruturas de Dados e Análises de Algoritmos/A3/src/Main.java
+++ Estruturas de Dados e Análises de Algoritmos/A3/src/Main.java
@@ -1,9 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Random random = new Random();
+
+        int[] data1000 = new int[1000];
+        int[] data5000 = new int[5000];
+        int[] data10000 = new int[10000];
+        int[] data50000 = new int[10000];
+        int[] data100000 = new int[100000];
+
 
     }
+
+    String bateriaTestes() {
+        return "";
+    }
+
 }
